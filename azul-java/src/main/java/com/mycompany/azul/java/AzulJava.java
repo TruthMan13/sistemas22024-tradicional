ef89ae65f1f295388e8ea4147da570b7b5b3efa1
Jeremy Gonzalez <jeremygonzalezg@gmail.com>
2023-03-16 06:09:57 +0100

    commit inicial

---
+++ azul-java/src/main/java/com/mycompany/azul/java/AzulJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.azul.java;
+
+/**
+ *
+ * @author jeremy
+ */
+public class AzulJava {
+
+    public static void main(String[] args) {
+        
+    }
+}
